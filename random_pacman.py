7651ebb0fb9b61053ae427fa3bf84739683dcccc
uidops <uidopsu@gmail.com>
2020-12-11 14:27:30 +0330

    add random cows and add the fortune

--- random_pacman.py
+++ random_pacman.py
@@ -3,12 +3,38 @@ import subprocess
 import random
 import os
 
-packages = subprocess.check_output("pacman -Q | cut -d ' ' -f1", shell=True)
-packages = packages.decode().split("\n")
-packages.pop()
+cows = [
+    "ghost",
+    "radio",
+    "pod",
+    "udder",
+    "milk",
+    "whale",
+    "sheep",
+    "dragon",
+    "homer",
+    "fox",
+    "wizard",
+    "taxi",
+    "blowfish.cow"
 
-random_package = random.choice(packages)
-data = subprocess.check_output('pacman -Qi {} | grep "Description" | cut -d ":" -f2'.format(random_package), shell=True)
 
-data = "{}:{}".format(random_package, data.decode())
-os.system("cowsay '{}'".format(data))
+]
+
+_ = [1,0]
+__ = random.choice(_)
+
+if __ == 1:
+    packages = subprocess.check_output("pacman -Q | cut -d ' ' -f1", shell=True)
+    packages = packages.decode().split("\n")
+    packages.pop()
+
+    random_package = random.choice(packages)
+    data = subprocess.check_output('pacman -Qi {} | grep "Description" | cut -d ":" -f2'.format(random_package), shell=True)
+
+    data = "{}:{}".format(random_package, data.decode())
+else:
+    data = subprocess.check_output("fortune", shell=True).decode()
+
+data = data.replace("\"", "\\\"")
+os.system("cowsay -f {} \"{}\"".format(random.choice(cows), data))
